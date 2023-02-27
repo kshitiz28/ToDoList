afddfc072cd1ac9e61d8cde3e0b77c48c2f344b3
kshitiz <kshitiz.dhakal50@gmail.com>
2023-02-27 21:10:21 +0545

    Add project files.

---
+++ To-Do List Management System/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace To_Do_List_Management_System_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ To-Do List Management System/Models/todo.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace To_Do_List_Management_System_.Models
+{
+    public class todo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string title { get; set; }
+
+        [Range(1,5,ErrorMessage="Criticality must be between 1 to 5."]
+        public int importance { get; set; }
+
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}

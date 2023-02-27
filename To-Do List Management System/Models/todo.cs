8fdf6b31990df186121d0200496f465b392c6842
kshitiz <kshitiz.dhakal50@gmail.com>
2023-02-27 21:14:11 +0545

    Connection string added for database.

--- To-Do List Management System/Models/todo.cs
+++ To-Do List Management System/Models/todo.cs
@@ -10,7 +10,7 @@ namespace To_Do_List_Management_System_.Models
         [Required]
         public string title { get; set; }
 
-        [Range(1,5,ErrorMessage="Criticality must be between 1 to 5."]
+        [Range(1,5,ErrorMessage="Criticality must be between 1 to 5 only !!")]
         public int importance { get; set; }
 
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;

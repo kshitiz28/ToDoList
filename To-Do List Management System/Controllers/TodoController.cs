d4462d906a885ef4341a4829c5fce0d032c48066
kshitiz <kshitiz.dhakal50@gmail.com>
2023-02-28 12:25:37 +0545

    Controller and view added for todo and layout edited.

---
+++ To-Do List Management System/Controllers/TodoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using To_Do_List_Management_System_.Data;
+
+namespace To_Do_List_Management_System_.Controllers
+{
+    public class TodoController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+
+        // db contains dbms information from program file which is assigned to _db to get the tables and data.
+        public TodoController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

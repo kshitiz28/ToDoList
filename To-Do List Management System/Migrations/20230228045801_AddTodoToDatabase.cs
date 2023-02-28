fcd7b796268401ca061a3d5cac76c1b8e4ed7df6
kshitiz <kshitiz.dhakal50@gmail.com>
2023-02-28 10:44:33 +0545

    Migrations added for todo class

---
+++ To-Do List Management System/Migrations/20230228045801_AddTodoToDatabase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace To_Do_List_Management_System_.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTodoToDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TODOs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    importance = table.Column<int>(type: "int", nullable: false),
+                    CreatedDateTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TODOs", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TODOs");
+        }
+    }
+}
